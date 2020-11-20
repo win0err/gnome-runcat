c0ff69db922c7eda9805c87dc5307be2b57a1097
Bouteiller (Aka. Hadock) Alan <bouteiller.alan@gmail.com>
2020-11-20 13:27:55 +0100

    ♻ fix(review): improve the settings code
    https://github.com/win0err/gnome-runcat/pull/6#discussion_r524300113

--- src/settings.js
+++ src/settings.js
@@ -1,40 +1,80 @@
 const Gio = imports.gi.Gio;
+const GioSSS = Gio.SettingsSchemaSource;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Extension = ExtensionUtils.getCurrentExtension();
 
-const SCHEMA_PATH = 'org.gnome.shell.extensions.se.kolesnikov.runcat';
+const SCHEMA_PATH = 'org.gnome.shell.extensions.runcat';
 
-function get_local_gsettings(schema_path) {
-    const GioSSS = Gio.SettingsSchemaSource;
+const valueTypes = {
+    INTEGER: 'int',
+    BOOLEAN: 'boolean',
+    STRING: 'string',
+    DOUBLE: 'double',
+}
 
-    const schemaDir = Extension.dir.get_child('schemas');
+var Settings = class Settings {
+    constructor(schemaPath = SCHEMA_PATH) {
+        const schemaDir = Extension.dir.get_child('schemas');
 
-    let schemaSource = GioSSS.get_default();
-    if (schemaDir.query_exists(null)) {
-        schemaSource = GioSSS.new_from_directory(
-            schemaDir.get_path(),
-            schemaSource,
-            false
-        );
+        let schemaSource = GioSSS.get_default();
+        if (schemaDir.query_exists(null)) {
+            schemaSource = GioSSS.new_from_directory(
+                schemaDir.get_path(),
+                schemaSource,
+                false
+            );
+        }
+
+        const schemaObj = schemaSource.lookup(schemaPath, true);
+        if (!schemaObj) {
+            throw new Error(`Schema ${schemaPath} could not be found for extension ${Extension.metadata.uuid}`);
+        }
+
+        this._gioSettings = new Gio.Settings({ settings_schema: schemaObj });
     }
 
-    const schemaObj = schemaSource.lookup(schema_path, true);
-    if (!schemaObj) {
-        throw new Error('Schema ' + schema_path + ' could not be found for extension ' + Extension.metadata.uuid);
+    get sleepingThreshold() {
+        if (!this._sleepingThreshold) {
+            this._sleepingThreshold = new Value(
+                this._gioSettings,
+                'sleeping-threshold',
+                valueTypes.INTEGER,
+            );
+        }
+
+        return this._sleepingThreshold;
+    }
+}
+
+class Value {
+    constructor(gioSettings, key, type) {
+        this._gioSettings = gioSettings;
+        this._key = key;
+        this._type = type;
+        this._connectedCallbacks = [];
     }
 
-    return new Gio.Settings({ settings_schema: schemaObj });
-};
+    set(v) {
+        return this._gioSettings[`set_${this._type}`](this._key, v);
+    }
 
-function Prefs() {
-    const settings = this.settings = get_local_gsettings(SCHEMA_PATH);
+    get() {
+        return this._gioSettings[`get_${this._type}`](this._key);
+    }
 
-    this.SLEEPINGTHRESHOLD = {
-        key: 'sleeping-threshold',
-        get: function () { return settings.get_double(this.key); },
-        set: function (v) { settings.set_double(this.key, v); },
-        changed: function (cb) { return settings.connect('changed::' + this.key, cb); },
-        disconnect: function () { return settings.disconnect.apply(settings, arguments); },
-    };
+    addListener(fn) {
+        const id = this._gioSettings.connect(`changed::${this._key}`, fn);
+        this._connectedCallbacks = [...this._connectedCallbacks, id];
+        return id;
+    }
 
-};
+    removeListener(id) {
+        this._gioSettings.disconnect(id);
+        this._connectedCallbacks = this._connectedCallbacks.filter(item => item !== id)
+    }
+
+    removeAllListeners() {
+        this._connectedCallbacks.forEach(id => this._gioSettings.disconnect(id));
+        this._connectedCallbacks = [];
+    }
+}
