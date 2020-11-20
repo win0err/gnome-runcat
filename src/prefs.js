5062c7b7d4a75b6af352e1680809b2c73d75e173
Sergei Kolesnikov <win0err@gmail.com>
2020-11-20 18:57:54 +0300

    Update prefs.js

--- src/prefs.js
+++ src/prefs.js
@@ -1,66 +1,87 @@
-const Gtk = imports.gi.Gtk;
-
+const { GObject, Gtk } = imports.gi;
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
+
 const { Settings } = Extension.imports.settings;
-const settings = new Settings();
 
-let sleepingThresholdSlider = null;
+const RuncatSettingsWidget = GObject.registerClass(
+    class RuncatSettingsWidget extends Gtk.Box {
+        _init() {
+            super._init({
+                orientation: Gtk.Orientation.VERTICAL,
+                border_width: 20,
+                spacing: 20
+            });
 
-function init() { }
+            this._settings = new Settings();
+
+            this._initSleepingThreshold();
+            this._initBottomButtons();
+
+            this.show_all();
+        }
+
+        _initSleepingThreshold() {
+            const hbox = new Gtk.Box({
+                orientation: Gtk.Orientation.HORIZONTAL,
+                spacing: 20,
+            });
+
+            const label = new Gtk.Label({
+                label: 'Sleeping Threshold',
+                use_markup: true,
+            });
+
+            const scale = new Gtk.HScale({
+                digits: 0,
+                adjustment: new Gtk.Adjustment({
+                    lower: 0,
+                    upper: 100,
+                }),
+                value_pos: Gtk.PositionType.RIGHT,
+                hexpand: true,
+                halign: Gtk.Align.END
+            });
+            scale.set_size_request(400, 15);
+            scale.set_value(this._settings.sleepingThreshold.get());
+
+            this._settings.sleepingThreshold.addListener (() => {
+                const updatedValue = this._settings.sleepingThreshold.get();
+                if (updatedValue !== scale.get_value()) {
+                    scale.set_value(updatedValue);
+                }
+            });
+            scale.connect('value-changed', () => {
+                const updatedValue = scale.get_value();
+                if (updatedValue !== this._settings.sleepingThreshold.get()) {
+                    this._settings.sleepingThreshold.set(scale.get_value());
+                }
+            });
+            this.connect('destroy', () => this._settings.sleepingThreshold.removeAllListeners());
+
+            hbox.add(label);
+            hbox.add(scale);
+
+            this.add(hbox);
+        }
+
+        _initBottomButtons() {
+            const resetButton = new Gtk.Button({
+                label: 'Reset to default',
+            });
+
+            resetButton.connect('clicked', () => {
+                this._settings.sleepingThreshold.set(0);
+            });
+
+            this.pack_end(resetButton, false, false, 0);
+        }
+    }
+);
 
 function buildPrefsWidget() {
-
-    const config = settings.sleepingThreshold;
-
-    const frame = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        border_width: 20,
-        spacing: 20
-    });
-
-    sleepingThresholdSlider = addSlider(frame, "Sleeping Threshold", config, 0, 99, 0);
-
-    addDefaultButton(frame, config);
-
-    frame.show_all();
-
-    return frame;
+    return new RuncatSettingsWidget();
 }
 
-function addDefaultButton(frame, config) {
-    const button = new Gtk.Button({label: "Reset to default"});
-    button.connect('clicked', function () {
-        config.set(0);
-        sleepingThresholdSlider.set_value(config.get());
-    });
-
-    frame.pack_end(button, false, false, 0);
-
-    return button;
-}
-
-function addSlider(frame, labelText, prefConfig, lower, upper, decimalDigits) {
-    const scale = new Gtk.HScale({
-        digits: decimalDigits,
-        adjustment: new Gtk.Adjustment({lower: lower, upper: upper}),
-        value_pos: Gtk.PositionType.RIGHT,
-        hexpand: true,
-        halign: Gtk.Align.END
-    });
-    scale.set_value(prefConfig.get());
-    scale.connect('value-changed', function (sw) {
-        const newval = sw.get_value();
-        if (newval !== prefConfig.get()) {
-            prefConfig.set(newval);
-        }
-    });
-    scale.set_size_request(400, 15);
-
-    const hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, spacing: 20});
-    hbox.add(new Gtk.Label({label: labelText, use_markup: true}));
-    hbox.add(scale);
-
-    frame.add(hbox);
-
-    return scale;
+function init() {
+    // do nothing
 }
