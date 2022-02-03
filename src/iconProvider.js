ada4b938d1b1822c6f65beda7c5655c96d5772ba
jafb321 <jafb321@gmail.com>
2022-02-02 18:17:18 -0700

    Add ability to set custom icon extension

--- src/iconProvider.js
+++ src/iconProvider.js
@@ -3,20 +3,20 @@ const ExtensionUtils = imports.misc.extensionUtils;
 
 const Extension = ExtensionUtils.getCurrentExtension();
 
-const getGIcon = (name, pack = 'cat') => Gio.icon_new_for_string(
-    `${Extension.path}/icons/${pack}/my-${name}-symbolic.svg`,
+const getGIcon = (name, pack = 'cat', ext = 'svg') => Gio.icon_new_for_string(
+    `${Extension.path}/icons/${pack}/my-${name}-symbolic.${ext}`,
 );
 
 // eslint-disable-next-line
 var IconProvider = class IconProvider {
-    constructor(pack = 'cat', spritesCount = 5) {
+    constructor(pack, ext, spritesCount = 5) {
         this.spritesCount = spritesCount;
         this.currentSprite = 0;
 
-        this._sleeping = getGIcon('sleeping', pack);
+        this._sleeping = getGIcon('sleeping', pack, ext);
 
         this.sprites = [...Array(spritesCount).keys()]
-            .map(i => getGIcon(`running-${i}`, pack));
+            .map(i => getGIcon(`running-${i}`, pack, ext));
     }
 
     get sleeping() {
@@ -37,5 +37,4 @@ var IconProvider = class IconProvider {
 
     reset() {
         this.currentSprite = 0;
-    }
-};
+    }};
