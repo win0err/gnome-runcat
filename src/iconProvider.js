3bceda92f43043bcc6cfd242128458897c520536
jafb321 <jafb321@gmail.com>
2022-02-02 18:26:16 -0700

    Add speed to Icon provider

--- src/iconProvider.js
+++ src/iconProvider.js
@@ -9,8 +9,9 @@ const getGIcon = (name, pack = 'cat', ext = 'svg') => Gio.icon_new_for_string(
 
 // eslint-disable-next-line
 var IconProvider = class IconProvider {
-    constructor(pack, ext, spritesCount = 5) {
+    constructor(pack, ext, spritesCount, speed) {
         this.spritesCount = spritesCount;
+        this.speed = speed;
         this.currentSprite = 0;
 
         this._sleeping = getGIcon('sleeping', pack, ext);
@@ -43,9 +44,9 @@ var IconProvider = class IconProvider {
 // eslint-disable-next-line
 var getIconProvider = (iconPack) => {
     const iconPacks = {
-        cat: new IconProvider('cat', 'svg', 5),
-        'dancing-parrot': new IconProvider('dancing-parrot', 'png', 10),
-        chicken: new IconProvider('chicken', 'gif', 12),
+        cat: new IconProvider('cat', 'svg', 5, 1),
+        'dancing-parrot': new IconProvider('dancing-parrot', 'png', 10, 1.5),
+        chicken: new IconProvider('chicken', 'gif', 12, 1.5),
     };
 
     return iconPacks[iconPack] || iconPack.cat;
